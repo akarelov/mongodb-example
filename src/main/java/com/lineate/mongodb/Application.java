226eef99f3949709e455aca9a6eb15aa2d545f8a
akarelov <karelov.artyom@gmail.com>
2020-01-22 17:33:13 +0400

    added bean for removing _class from document

--- src/main/java/com/lineate/mongodb/Application.java
+++ src/main/java/com/lineate/mongodb/Application.java
@@ -1,11 +1,34 @@
 package com.lineate.mongodb;
 
+import com.mongodb.MongoClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.MongoDbFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
+import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
+import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
 
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
+    MongoDbFactory mongoDbFactory() {
+        return new SimpleMongoDbFactory(new MongoClient(), "test_database");
+    }
+
+    @Bean
+    public MongoTemplate mongoTemplate() {
+        //remove _class
+        MappingMongoConverter converter = new MappingMongoConverter(mongoDbFactory(), new MongoMappingContext());
+        converter.setTypeMapper(new DefaultMongoTypeMapper(null));
+
+        return new MongoTemplate(mongoDbFactory(), converter);
+
+    }
+
 }
